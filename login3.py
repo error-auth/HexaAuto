a39e90968f43f8d657754a98864b2921fa94bb26
error-auth <auth.2418362@gmail.com>
2024-08-01 10:40:25 +0530

    Add files via upload

---
+++ login3.py
@@ -0,0 +1,20 @@
+import asyncio
+import re
+from telethon import TelegramClient, events
+from telethon.errors import MessageIdInvalidError
+
+api_id = 4608923
+api_hash = '0fc54e6096c9cd77cd1e1954b899676d'
+
+async def main():
+    client = TelegramClient('account3', api_id, api_hash)
+    await client.start()
+
+    print('''
+          Acoount Logged in
+                                 ~ paradox  ''')
+    
+
+    await client.run_until_disconnected()
+
+asyncio.run(main())
